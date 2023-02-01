d2bdbcc96397d452eccf667903423c8f8fdaaca3
Isabelle Halliday <isabellehalliday@Isabelles-iMac.local>
2023-02-01 09:38:57 +0000

    newchanges

--- udemy/hangman/requests.js
+++ udemy/hangman/requests.js
@@ -1,4 +1,16 @@
-const getPuzzle = (wordCount) => {
+const getPuzzle = async (wordCount) => {
+  const response = await fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+  
+  if (response.status === 200) {
+const data = await response.json()
+return data.puzzle
+  } else {
+    throw new Error('Unable to get puzzle')
+  }
+};
+//////////////////////////
+
+const getPuzzleOld = (wordCount) => {
   return fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
     .then((response) => {
       if (response.status === 200) {
@@ -12,6 +24,9 @@ const getPuzzle = (wordCount) => {
     });
 };
 //////////////////////////
+
+
+
 
 const getCountry = (countryCode) => {
   return fetch(`https://restcountries.com/v2/all?countryCode=${countryCode}`)
