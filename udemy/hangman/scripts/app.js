b30214677fb4112bcec092fdf5e921a2eaa0ad72
Isabelle Halliday <isabellehalliday@Isabelles-iMac.local>
2023-02-01 11:28:31 +0000

    newfiles

--- udemy/hangman/scripts/app.js
+++ udemy/hangman/scripts/app.js
@@ -15,8 +15,16 @@ window.addEventListener("keypress", (e) => {
 //////////////
 
 const render = () => {
-  puzzleEl.textContent = game1.puzzle;
+  puzzleEl.innerHTML = ''
+  // puzzleEl.textContent = game1.puzzle;
   guessesEl.textContent = game1.statusMessage;
+
+  game1.puzzle.split('').forEach((letter) => {
+const letterEl = document.createElement('span')
+letterEl.textContent = letter
+puzzleEl.appendChild(letterEl)
+  })
+
 }
 
 const startGame = async () => {
